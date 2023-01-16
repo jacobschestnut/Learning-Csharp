c1c6c50491df0ba81ea3a0a48931f628d101a26e
Jake Chestnut <jacobschestnut@gmail.com>
2023-01-16 11:12:18 -0600

    init commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string greeting = "Hello";
+Console.WriteLine(greeting);
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+Console.WriteLine("Hello there, " + name);
